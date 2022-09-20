503bd407e14b6b9883d7992dcb11e8ab5e7e3555
Drobarov12 <drobarovmartin@yahoo.com>
2022-09-20 21:59:54 +0200

    ReservationServise implemented

--- Hotel/src/main/java/com/example/hotel/service/forms/RoomForm.java
+++ Hotel/src/main/java/com/example/hotel/service/forms/RoomForm.java
@@ -2,11 +2,8 @@ package com.example.hotel.service.forms;
 
 import com.example.hotel.domain.valueobjects.*;
 import com.example.hotel.domain.valuesOf.MoneyCurrency;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-import java.time.LocalDateTime;
 import javax.validation.constraints.*;
 
 
@@ -17,6 +14,9 @@ public class RoomForm {
     @NotNull
     @Min(1)
     private int numOfBeds;
+    @NotNull
+    @Min(1)
+    private int numOfPeople;
     @NotNull
     @Min(5)
     private int pricePerBed;
